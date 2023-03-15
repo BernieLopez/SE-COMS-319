d029c5c6878a95afa71896f90065c7907109ccf5
BernieLopez <picnicbasketsam@gmail.com>
2023-03-15 18:38:38 -0500

    more in line with project requirements

---
+++ midterm/index.js
@@ -0,0 +1,4 @@
+/*
+data.json is supposed to contain all the text that gets loaded into the page. This file is supposed to
+have the programming that loads that text, as well as the images, into the webpages.
+*/
