8a97222f4fe21a6f0d02199a597c50439b31dbd4
BernieLopez <picnicbasketsam@gmail.com>
2023-03-17 17:13:17 -0500

    started js implementation

--- midterm/index.js
+++ midterm/index.js
@@ -1,4 +1,13 @@
 /*
-data.json is supposed to contain all the text that gets loaded into the page. This file is supposed to
+data.json contains all the text that gets loaded into the page. This file is supposed to
 have the programming that loads that text, as well as the images, into the webpages.
 */
+window.addEventListener("DOMContentLoaded", domLoaded);
+
+function domLoaded() {
+    let civicPic = document.getElementById("civic");
+    civicPic.innerHTML += `<img src="./images/civic.png" alt="Honda Civic"></img>`;
+
+    let civicText = document.getElementById("civic-details");
+    
+}
